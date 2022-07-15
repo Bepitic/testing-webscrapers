ad2d52250c332d96ea5b6807f2a03e8ea9ce1f9b
Bepitic <bep@bepitic.com.es>
2022-07-15 16:44:03 +0200

    test2

---
+++ 2test/urlget.py
@@ -0,0 +1,10 @@
+""" test2 getting a webpage """
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.pythonscraping.com/pages/page3.html")
+bs_obj = BeautifulSoup(html)
+
+for sibling in bs_obj.find("table", {"id":"giftList"}).tr:
+    print(sibling)
