edf5618a7720648f612b91e3cfa83839248ec25c
Bepitic <bepitic@gmail.com>
2022-07-26 15:29:51 +0200

    good runner bbc WIP the scraping part

--- 5test-scrapy/Scrapy_recipes/Recipes/Recipes/spiders/bbc_recipes.py
+++ 5test-scrapy/Scrapy_recipes/Recipes/Recipes/spiders/bbc_recipes.py
@@ -10,13 +10,14 @@ class BbcRecipesSpider(CrawlSpider):
 
     # TODO: Add a new rule 4 crawl everything of the page
     rules = (
-        Rule(LinkExtractor(allow=r'recipes/',deny=r'recipes/collection/'), callback='parse_recipe', follow=True),
+        Rule(LinkExtractor(allow=(r'collection',r'category'))),
+        Rule(LinkExtractor(allow=r'recipes/',deny=(r'collection',r'category')), callback='parse_recipe', follow=True),
     )
 
     def parse_recipe(self, response):
         item = {}
         item['title'] = response.css('div.headline h1::text').get()
         recipe = response.css('div.recipe__instructions').get()
-        item['ingredients'] = recipe.css('').get()
-        item['instrucctions'] = recipe.css('').get()
+        #item['ingredients'] = recipe.css('').get()
+        #item['instrucctions'] = recipe.css('').get()
         return item
