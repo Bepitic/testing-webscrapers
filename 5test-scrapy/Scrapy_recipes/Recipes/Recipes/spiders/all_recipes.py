fae5e283e7f7f7b8546ccb514cca7ba0ac651ced
Bepitic <bepitic@gmail.com>
2022-08-01 11:50:44 +0200

    crawler for allrecipes

--- 5test-scrapy/Scrapy_recipes/Recipes/Recipes/spiders/all_recipes.py
+++ 5test-scrapy/Scrapy_recipes/Recipes/Recipes/spiders/all_recipes.py
@@ -9,12 +9,23 @@ class AllRecipesSpider(CrawlSpider):
     start_urls = ['http://www.allrecipes.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'recipe/'), callback='parse_recipe', follow=True),
     )
+
+    def parse_recipe(self, response):
+        item = {}
+        item['title'] = response.css('div.main-header div.intro ::text').get()
+        recipe = response.css('div.recipe__instructions').get()
+        unclean_ingredients = response.css('ul.ingredients-section ::text').getall()
+        item['ingredients'] = list(filter(lambda x: x != ' ', unclean_ingredients))
+
+        unclean_instructions = response.css('section.recipe-instructions ::text').getall()
+        item['instructions'] = list(filter(lambda x: x != ' ', unclean_instructions))
+        return item
 
     def parse_item(self, response):
         item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
+        item[''] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
         return item
